c8af09008d0dc54ba354e2b62296a166312d69a1
AmalShiwantha <amal.s@aeturnum.com>
2022-01-19 15:56:02 +0530

    setup mvvm directires

--- app/src/main/java/com/ceylonapz/weatherlive/view/activity/ScrollingActivity.kt
+++ app/src/main/java/com/ceylonapz/weatherlive/view/activity/ScrollingActivity.kt
@@ -1,12 +1,11 @@
-package com.ceylonapz.weatherlive
+package com.ceylonapz.weatherlive.ui
 
 import android.os.Bundle
-import com.google.android.material.appbar.CollapsingToolbarLayout
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.ceylonapz.weatherlive.R
 import com.ceylonapz.weatherlive.databinding.ActivityScrollingBinding
 
 class ScrollingActivity : AppCompatActivity() {
