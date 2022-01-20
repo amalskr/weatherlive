9b9c1abf6254d1f36bc37b8d4cb89dafbb1fe356
AmalShiwantha <amal.s@aeturnum.com>
2022-01-20 20:04:25 +0530

    databind to the details view

--- app/src/main/java/com/ceylonapz/weatherlive/view/activity/DetailsActivity.kt
+++ app/src/main/java/com/ceylonapz/weatherlive/view/activity/DetailsActivity.kt
@@ -1,19 +1,26 @@
 package com.ceylonapz.weatherlive.view.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.ceylonapz.weatherlive.R
+import androidx.appcompat.app.AppCompatActivity
+import com.ceylonapz.weatherlive.databinding.ActivityDetailsBinding
+import com.ceylonapz.weatherlive.model.Days
 import com.ceylonapz.weatherlive.utilities.SELECTED_FORECAST_DAY
 
 class DetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         getIntentData()
     }
 
     private fun getIntentData() {
-        val selectedForecastDay = intent.getSerializableExtra(SELECTED_FORECAST_DAY)
+        val selectedForecastDay = intent.getSerializableExtra(SELECTED_FORECAST_DAY) as Days
+        binding.viewDay = selectedForecastDay
     }
 }
